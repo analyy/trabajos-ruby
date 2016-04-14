238a054e953e686a395596ac754c17c00513c511
Analy <analy_gonzales95@outlook.com>
2016-04-13 22:45:02 -0500

    proyecto-ruby

---
+++ ejercicio02.rb
@@ -0,0 +1,11 @@
+i = 0
+
+while  i<20
+    i+=1
+	
+	puts i
+end
+
+
+
+ gets()
---
+++ ejercicio03.rb
@@ -0,0 +1,10 @@
+paises=["peru","colombia","francia","venezuela","brasil"]
+
+
+puts "mi pais  favorito es :" + paises [0]
+puts "mi pais  favorito es :" + paises [1]
+puts "mi pais  favorito es :" + paises [3]
+puts "mi pais  favorito es :" + paises [4]
+
+
+gets()
